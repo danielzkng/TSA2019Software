0c9c4659225a4313c570051cbb3a23e9a9316917
allandao <38929333+allandao@users.noreply.github.com>
2018-12-27 18:17:23 -0800

    Basic Resources - Minor

--- Software_Development/Software_Development/Properties/Resources.Designer.cs
+++ Software_Development/Software_Development/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace Software_Development.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap xicon {
+            get {
+                object obj = ResourceManager.GetObject("xicon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
